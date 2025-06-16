a9f6f22e653c52c3eb77abbd1074831155e7e554
Martin-Rouault <martin.rlt24@gmail.com>
2025-06-16 17:17:49 +0200

    add delete route

--- app.js
+++ app.js
@@ -9,3 +9,43 @@ app.get("/",(req, res) => {
 app.listen(port, () => {
 	console.log(`server is running on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.delete("/delete/:id", (req, res) => {
+	try {
+		const id =  req.params.id;
+		res.send(`L'utilisateur avec pour id: ${id} a été supprimé`);
+	} catch (error) {
+		console.error(error)	
+		res.status(500).send("Une erreur est survenue.");	
+	}
+});
+
