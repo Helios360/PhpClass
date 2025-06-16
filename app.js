fe703080cddfff9f1908e4f6d5d85038cf865a0a
Helios360 <elias.treguier@gmail.com>
2025-06-16 14:48:58 +0200

    changes

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/",(req, res) => {
 	res.send("hello world");
 });
 
+app.get("/date",(req, res) => {
+	res.send(`Current date and time: ${new Date()}`);
+});
+
 app.listen(port, () => {
-	console.log("server is running on port ${port} ");
+	console.log(`server is running on port ${port} `);
 });
